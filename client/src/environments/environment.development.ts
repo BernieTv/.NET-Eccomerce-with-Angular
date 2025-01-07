716b5415acc8edae6e7e8afdcfc894a7a15549ab
Bekzod Tukhtasinov <bektuxtasinov@gmail.com>
2025-01-07 19:45:58 +0500

    deploy to azure

---
+++ client/src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://localhost:5001/api/',
+  hubUrl: 'https://localhost:5001/hub/notifications',
+  stripePublicKey:
+    'pk_test_51QeCyMIEz8ywySUG81G27K7GGSQlXhtk1COWbftsKpCPBzwnzqSjFBoHKUPrwcE7HgrLbSSNagnr2KDrUvVHrrhQ00IwnD8CfZ',
+};
---
+++ client/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiUrl: 'api/',
+  hubUrl: 'hub/notifications',
+  stripePublicKey:
+    'pk_test_51QeCyMIEz8ywySUG81G27K7GGSQlXhtk1COWbftsKpCPBzwnzqSjFBoHKUPrwcE7HgrLbSSNagnr2KDrUvVHrrhQ00IwnD8CfZ',
+};
